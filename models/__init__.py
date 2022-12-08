13a2144153fcc4909e50cdd8cea73ba572f97925
InfotecLaPlata <lucasyurkiw@gmail.com>
2022-12-08 14:37:58 -0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,5 @@
-# -*- coding: utf-8 -*-
-
-from . import models
+from . import delivery_carrier
+from . import product_template
+from . import res_company
+from . import sale_order
+from . import zippin_pickup_points
