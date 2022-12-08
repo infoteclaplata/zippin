2ebfa6c497eed9b08a307fd335f05b1ad0718dcb
InfotecLaPlata <lucasyurkiw@gmail.com>
2022-12-08 14:38:04 -0300

    Create delivery_carrier.py

---
+++ models/delivery_carrier.py
@@ -0,0 +1,202 @@
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+from requests.structures import CaseInsensitiveDict
+import requests, base64
+
+APIURL= "https://api.zippin.com.ar/v2"
+ID_CORREO_ARGENTINO = 233
+ID_OCA = 208
+ID_ANDREANI = 1
+ID_STANDARD_DELIVERY = 1
+ID_PICKUP_DELIVERY = 9
+
+class DeliveryCarrier(models.Model):
+    _inherit = 'delivery.carrier'
+
+    delivery_type = fields.Selection(selection_add=[('zippin', 'Zippin')], ondelete={'zippin': 'set default'})
+    zippin_pickup = fields.Many2one('zippin.pickup.points', string="Sucursales")
+
+    def _zippin_api_headers(self, order):
+
+        headers = CaseInsensitiveDict()
+        headers["Content-Type"] = "application/json"
+        headers["Accept"] = "application/json"
+
+        zippin_auth = "%s:%s" % (order.company_id.zippin_key, order.company_id.zippin_secret)
+        zippin_auth = base64.b64encode(zippin_auth.encode("utf-8")).decode("utf-8")
+
+        headers["Authorization"] = "Basic " + zippin_auth
+
+        return(headers)
+
+    def _zippin_get_origen_id(self, order):
+
+        url = APIURL + "/addresses?account_id=" + order.company_id.zippin_id
+
+        r = requests.get(url, headers=self._zippin_api_headers(order))
+        r = r.json()
+
+        for i in r["data"]:
+            if i["id"]:
+               resp = i["id"]
+
+        return(resp)
+
+    def _zippin_prepare_items(self, order):
+
+        if order.order_line:
+            r = []
+
+            for p in order.order_line:
+                if p.product_type != 'service' and p.product_type != 'consu':
+                    if p.product_id.weight == False or p.product_id.product_height == False or p.product_id.product_width == False or p.product_id.product_length == False:
+                        raise ValidationError('Error: El producto ' + p.product_id.name + ' debe tener peso y tamaño asignados.')
+
+                    for i in range(int(p.product_uom_qty)):
+                        product_list = {
+                          "weight": p.product_id.weight * 1000,
+                          "height": p.product_id.product_height,
+                          "width": p.product_id.product_width,
+                          "length": p.product_id.product_length,
+                          "description": p.product_id.name,
+                          "classification_id": 1
+                        }
+
+                        r.append(product_list)
+
+        return(r)
+
+    def _zippin_to_shipping_data(self, order):
+
+        if order.partner_shipping_id.city == False:
+            raise ValidationError('¡El Cliente debe tener Ciudad!')
+        elif order.partner_shipping_id.state_id.name == False:
+            raise ValidationError('¡El Cliente debe tener Estado/Provincia!')
+        elif order.partner_shipping_id.zip == False:
+            raise ValidationError('¡El Cliente debe tener Codigo Postal!')
+        else:
+            r = {
+                "city": order.partner_shipping_id.city,
+                "state": order.partner_shipping_id.state_id.name,
+                "zipcode": order.partner_shipping_id.zip,
+            }
+        return(r)
+
+    def zippin_rate_shipment(self, order):
+
+        url = APIURL + "/shipments/quote"
+
+        #VALOR DECLARADO EN CERO SI NO SE PONE SEGURO AL ENVIO
+        data = {
+            "account_id": order.company_id.zippin_id,
+            "origin_id": self._zippin_get_origen_id(order),
+            "declared_value": 0,
+        }
+
+        data["items"] = self._zippin_prepare_items(order)
+
+        data["destination"]= self._zippin_to_shipping_data(order)
+
+        r = requests.post(url, headers=self._zippin_api_headers(order), json=data)
+
+        if r.status_code == 200:
+            shipment_price = 0
+            r= r.json()
+            pickup_res = []
+            pickup_address = ''
+
+            for i in r["all_results"]:
+                if self.product_id.zippin_shipment_type == 'zippin_car_suc':
+                    if i["carrier"]["id"] == ID_CORREO_ARGENTINO and i["service_type"]["id"] == ID_PICKUP_DELIVERY:
+                        shipment_price = i["amounts"]["price"]
+                        logistic_type = i["logistic_type"]
+
+                elif self.product_id.zippin_shipment_type == 'zippin_car_dom':
+                    if i["carrier"]["id"] == ID_CORREO_ARGENTINO and i["service_type"]["id"] == ID_STANDARD_DELIVERY:
+                        shipment_price = i["amounts"]["price"]
+                        logistic_type = i["logistic_type"]
+
+                elif self.product_id.zippin_shipment_type == 'zippin_oca_suc':
+                    if i["carrier"]["id"] == ID_OCA and i["service_type"]["id"] == ID_PICKUP_DELIVERY:
+                        shipment_price = i["amounts"]["price"]
+                        logistic_type = i["logistic_type"]
+
+                elif self.product_id.zippin_shipment_type == 'zippin_oca_dom':
+                    if i["carrier"]["id"] == ID_OCA and i["service_type"]["id"] == ID_STANDARD_DELIVERY:
+                        shipment_price = i["amounts"]["price"]
+                        logistic_type = i["logistic_type"]
+
+                elif self.product_id.zippin_shipment_type == 'zippin_and_suc':
+                    if i["carrier"]["id"] == ID_ANDREANI and i["service_type"]["id"] == ID_PICKUP_DELIVERY:
+                        shipment_price = i["amounts"]["price"]
+                        logistic_type = i["logistic_type"]
+
+                elif self.product_id.zippin_shipment_type == 'zippin_and_dom':
+                    if i["carrier"]["id"] == ID_ANDREANI and i["service_type"]["id"] == ID_STANDARD_DELIVERY:
+                        shipment_price = i["amounts"]["price"]
+                        logistic_type = i["logistic_type"]
+
+                if i["service_type"]["id"] == ID_PICKUP_DELIVERY:
+                    for f in i["pickup_points"]:
+                        pickup_points = {
+                            "order_id": order.id,
+                            "carrier_id": i["carrier"]["id"],
+                            "point_id": f["point_id"],
+                            "name": f["description"],
+                            "address": f["location"]["street"] + ' ' + f["location"]["street_number"] + ' ' + f["location"]["city"] + ' ' + f["location"]["state"],
+                            "logistic_type": i["logistic_type"]
+                        }
+                        pickup_res.append(pickup_points)
+
+            if shipment_price != 0:
+                return {
+                    'success': True,
+                    'price': shipment_price,
+                    'zippin_pickup': pickup_res,
+                    'logistic_type': logistic_type,
+                    'error_message': False,
+                    'warning_message': False
+                }
+            else:
+                return {
+                    'success': False,
+                    'price': shipment_price,
+                    'error_message': 'No disponible'
+                }
+
+        elif r.status_code == 401:
+            return {
+                'success': False,
+                'price': 0,
+                'error_message': 'Error de autorización, revise sus credenciales.',
+                'warning_message': False
+            }
+        elif r.status_code == 408:
+            return {
+                'success': False,
+                'price': 0,
+                'error_message': 'Error. La solicitud está tomando demasiado tiempo en procesarse, intente nuevamente.',
+                'warning_message': False
+            }
+        elif r.status_code == 500:
+            return {
+                'success': False,
+                'price': 0,
+                'error_message': 'Error interno. Intente nuevamente.',
+                'warning_message': False
+            }
+        elif r.status_code == 503:
+            return {
+                'success': False,
+                'price': 0,
+                'error_message': 'Error interno. El servidor se encuentra saturado, espere unos minutos y vuelva a intentarlo.',
+                'warning_message': False
+            }
+        else:
+            data = r.json()
+            return {
+                'success': False,
+                'price': 0,
+                'error_message': 'No disponible',
+                'warning_message': False
+            }
