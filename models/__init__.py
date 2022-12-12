9ae3609cb16e8ac6acffb85cf59e4ef290844e91
InfotecLaPlata <lucasyurkiw@gmail.com>
2022-12-12 16:12:48 -0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,4 @@
 from . import delivery_carrier
-from . import product_template
 from . import res_company
 from . import sale_order
-from . import zippin_pickup_points
+from . import zippin_shipping
