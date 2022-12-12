0ab077e95593347c5f15fca390425956020c60e0
InfotecLaPlata <lucasyurkiw@gmail.com>
2022-12-12 16:16:20 -0300

    Update delivery_carrier.py

--- models/delivery_carrier.py
+++ models/delivery_carrier.py
@@ -14,7 +14,16 @@ class DeliveryCarrier(models.Model):
     _inherit = 'delivery.carrier'
 
     delivery_type = fields.Selection(selection_add=[('zippin', 'Zippin')], ondelete={'zippin': 'set default'})
-    zippin_pickup = fields.Many2one('zippin.pickup.points', string="Sucursales")
+    zippin_pickup = fields.Many2one('zippin.shipping', string="Sucursales")
+    zippin_shipment = fields.Boolean('Conectar con Zippin',help='Conecta el proveedor de envíos con Zippin',index=True)
+    zippin_shipment_type = fields.Selection([
+            (str(ID_CORREO_ARGENTINO), "Correo Argentino"), 
+            (str(ID_OCA), "OCA"),
+            (str(ID_ANDREANI), "Andreani"),
+        ],
+        string="Seleccionar Proveedor y Tipo de Envío",
+    )
+    zippin_shipment_type_is_pickup = fields.Boolean('Es envio a Sucursal')
 
     def _zippin_api_headers(self, order):
 
@@ -34,13 +43,15 @@ class DeliveryCarrier(models.Model):
         url = APIURL + "/addresses?account_id=" + order.company_id.zippin_id
 
         r = requests.get(url, headers=self._zippin_api_headers(order))
-        r = r.json()
 
-        for i in r["data"]:
-            if i["id"]:
-               resp = i["id"]
-
-        return(resp)
+        if r.status_code == 403:
+            raise ValidationError('Zippin: Error de autorización, revise sus credenciales.')
+        else:
+            r = r.json()
+            for i in r["data"]:
+                if i["id"]:
+                   resp = i["id"]
+            return(resp)
 
     def _zippin_prepare_items(self, order):
 
@@ -75,10 +86,22 @@ class DeliveryCarrier(models.Model):
         elif order.partner_shipping_id.zip == False:
             raise ValidationError('¡El Cliente debe tener Codigo Postal!')
         else:
+            zp_phone = ''
+            if order.partner_shipping_id.phone:
+                zp_phone = zp_phone + order.partner_shipping_id.phone
+            elif order.partner_shipping_id.mobile:
+                zp_phone = ' - ' + order.partner_shipping_id.mobile
             r = {
                 "city": order.partner_shipping_id.city,
                 "state": order.partner_shipping_id.state_id.name,
                 "zipcode": order.partner_shipping_id.zip,
+                "name": order.partner_shipping_id.name,
+                "document": order.partner_shipping_id.vat,
+                "street": order.partner_shipping_id.street,
+                "street_number": order.partner_shipping_id.street2,
+                "street_extras": '',
+                "phone": zp_phone,
+                "email": order.partner_shipping_id.email,
             }
         return(r)
 
@@ -87,6 +110,9 @@ class DeliveryCarrier(models.Model):
         url = APIURL + "/shipments/quote"
 
         #VALOR DECLARADO EN CERO SI NO SE PONE SEGURO AL ENVIO
+        if order.company_id.zippin_key == False or order.company_id.zippin_id == False or order.company_id.zippin_secret == False:
+            raise ValidationError('Debe ingresar las credenciales de Zippin en ajustes de la Empresa')
+
         data = {
             "account_id": order.company_id.zippin_id,
             "origin_id": self._zippin_get_origen_id(order),
@@ -104,35 +130,15 @@ class DeliveryCarrier(models.Model):
             r= r.json()
             pickup_res = []
             pickup_address = ''
+            logistic_type = ''
 
             for i in r["all_results"]:
-                if self.product_id.zippin_shipment_type == 'zippin_car_suc':
-                    if i["carrier"]["id"] == ID_CORREO_ARGENTINO and i["service_type"]["id"] == ID_PICKUP_DELIVERY:
+                if self.zippin_shipment_type_is_pickup:
+                    if i["carrier"]["id"] == int(self.zippin_shipment_type) and i["service_type"]["id"] == ID_PICKUP_DELIVERY:
                         shipment_price = i["amounts"]["price"]
                         logistic_type = i["logistic_type"]
-
-                elif self.product_id.zippin_shipment_type == 'zippin_car_dom':
-                    if i["carrier"]["id"] == ID_CORREO_ARGENTINO and i["service_type"]["id"] == ID_STANDARD_DELIVERY:
-                        shipment_price = i["amounts"]["price"]
-                        logistic_type = i["logistic_type"]
-
-                elif self.product_id.zippin_shipment_type == 'zippin_oca_suc':
-                    if i["carrier"]["id"] == ID_OCA and i["service_type"]["id"] == ID_PICKUP_DELIVERY:
-                        shipment_price = i["amounts"]["price"]
-                        logistic_type = i["logistic_type"]
-
-                elif self.product_id.zippin_shipment_type == 'zippin_oca_dom':
-                    if i["carrier"]["id"] == ID_OCA and i["service_type"]["id"] == ID_STANDARD_DELIVERY:
-                        shipment_price = i["amounts"]["price"]
-                        logistic_type = i["logistic_type"]
-
-                elif self.product_id.zippin_shipment_type == 'zippin_and_suc':
-                    if i["carrier"]["id"] == ID_ANDREANI and i["service_type"]["id"] == ID_PICKUP_DELIVERY:
-                        shipment_price = i["amounts"]["price"]
-                        logistic_type = i["logistic_type"]
-
-                elif self.product_id.zippin_shipment_type == 'zippin_and_dom':
-                    if i["carrier"]["id"] == ID_ANDREANI and i["service_type"]["id"] == ID_STANDARD_DELIVERY:
+                else:
+                    if i["carrier"]["id"] == int(self.zippin_shipment_type) and i["service_type"]["id"] == ID_STANDARD_DELIVERY:
                         shipment_price = i["amounts"]["price"]
                         logistic_type = i["logistic_type"]
 
@@ -147,30 +153,15 @@ class DeliveryCarrier(models.Model):
                             "logistic_type": i["logistic_type"]
                         }
                         pickup_res.append(pickup_points)
-
-            if shipment_price != 0:
-                return {
-                    'success': True,
-                    'price': shipment_price,
-                    'zippin_pickup': pickup_res,
-                    'logistic_type': logistic_type,
-                    'error_message': False,
-                    'warning_message': False
-                }
-            else:
-                return {
-                    'success': False,
-                    'price': shipment_price,
-                    'error_message': 'No disponible'
-                }
-
-        elif r.status_code == 401:
             return {
-                'success': False,
-                'price': 0,
-                'error_message': 'Error de autorización, revise sus credenciales.',
+                'success': True,
+                'price': shipment_price,
+                'zippin_pickup': pickup_res,
+                'logistic_type': logistic_type,
+                'error_message': False,
                 'warning_message': False
             }
+
         elif r.status_code == 408:
             return {
                 'success': False,
